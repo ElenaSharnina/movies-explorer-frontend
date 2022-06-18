ca82e96443532503feebad4975c97bd8fc037143
ElenaSharnina <92175599+ElenaSharnina@users.noreply.github.com>
2022-06-18 22:15:08 +0300

    aboutProject

---
+++ src/components/Main/AboutProject/AboutProject.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+function AboutProject() {
+  return (
+    <div className="aboutproject page__container">
+      <h2 className="aboutproject__title">О проeкте</h2>
+      <ul className="aboutproject__two-columns">
+        <li className="aboutproject__column">
+          <h3 className="aboutproject__column-title">Дипломный проект включал 5 этапов</h3>
+          <p className="aboutpriject__description">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
+        </li>
+        <li className="aboutproject__column">
+          <h3 className="aboutproject__column-title">На выполнение диплома ушло 5 недель</h3>
+          <p className="aboutpriject__description">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
+        </li>
+      </ul>
+      <ul className="aboutproject__timing">
+        <li className="aboutproject__timing-item">
+          <h4 className="aboutproject__timing-title aboutproject__timing-title_color_green">1 неделя</h4>
+          <p className="aboutproject__timing-desc">Back-end</p>
+        </li>
+        <li className="aboutproject__timing-item">
+          <h4 className="aboutproject__timing-title">4 недели</h4>
+          <p className="aboutproject__timing-desc">Front-end</p>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default AboutProject;
--- src/components/Main/Promo/Promo.js
+++ src/components/Main/Promo/Promo.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Promo() {
   return (
-    <div className="promo page__container">
+    <div className="promo">
       <h1 className="promo__title">Учебный проект студента факультета Веб-разработки.</h1>
       <nav className="promo__nav">
         <button className="button promo__button">О проекте</button>
